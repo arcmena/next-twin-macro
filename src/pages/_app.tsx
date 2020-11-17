ffe6c733657a648a44d154b11d15173636049ef3
Marcelo Mena <marcelomena010@gmail.com>
2020-11-16 22:38:19 -0300

    Create color theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import { GlobalStyles } from 'twin.macro'
+import { ThemeProvider } from 'styled-components'
+
+// Components
 import DefaultLayout from '../components/layouts/DefaultLayout'
+
+import theme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <GlobalStyles />
             <DefaultLayout>
                 <Component {...pageProps} />
             </DefaultLayout>
-        </>
+        </ThemeProvider>
     )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+    colors: {
+        white: '#fbfbfb',
+        gray: '#2d3748',
+        blue: '#2b6cb0',
+        blue_1: '#2c5282'
+    }
+}
+
+export default theme
